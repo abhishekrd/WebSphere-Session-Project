2398fe2310bcddfd732845a29bd54225b978d7d5
abhishekrd <abhishekrdhanke636@gmail.com>
2023-03-31 13:48:44 +0530

    initial commit :)

---
+++ script.js
@@ -0,0 +1,25 @@
+var msg_one = document.getElementById("msg_one");
+var msg_two = document.getElementById("msg_two");
+var count = 0;
+var ans =  (Math.random()*100)
+console.log(ans);
+
+function decider(){
+    var inputNum = document.getElementById("inputNum").value;
+
+    count = count + 1;
+
+    if(inputNum == ans){
+        window.alert("CONGRATULATIONS!! You guessed it RIGHT✅");
+        msg_one.textContent = "You guessed it rightly after : " + count + " guesses";
+        msg_two.textContent = "Congratulations, you have guessed it right!"
+    }
+    else if(inputNum < ans){
+        msg_one.textContent = "Number of guesses : " + count;
+        msg_two.textContent = "Please enter a larger Number";
+    }
+    else{
+        msg_one.textContent = "Number of guesses : " + count; 
+        msg_two.textContent = "Please enter a smaller number";
+    }
+}
